911ec3c5941f16c0b3413bce4924bae0dc3c7ed2
Gerard Devine <g.devine@uws.edu.au>
2015-08-31 12:49:40 +1000

    Access details moved to environment variables

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 HIEv - List users
 ============
 
-Python script to perform a webscrape of the HIEv user list and return a csv formatte file of the informations
+Python script to perform a webscrape of the HIEv user list and return a csv formatted file of the informations
 
-
+Admin email and password need to be set locally under environment variables 'AdminEmail' and 'AdminPass' 
--- hiev_list_users.py
+++ hiev_list_users.py
@@ -9,6 +9,7 @@ Date: August 2015
 
 '''
 
+import os
 import mechanize
 import cookielib
 from BeautifulSoup import BeautifulSoup
@@ -38,9 +39,9 @@ br.open('https://hiev.uws.edu.au/users/sign_in')
 # Select the actual login form in the html
 br.select_form(nr=0)
 
-# Fill in user email and password
-br.form['user[email]'] = 'g.devine@uws.edu.au'
-br.form['user[password]'] = 'Altinure_1'
+# Fill in user email and password via preset environment variables 
+br.form['user[email]'] = os.environ['AdminEmail']
+br.form['user[password]'] = os.environ['AdminPass']
 
 # login to the site
 br.submit()
@@ -49,13 +50,13 @@ soup = BeautifulSoup(html_text)
 
 # scrape the user information and write it out to datestamped csv file 
 with open('hiev_userlist_'+datetime.now().strftime('%Y%m%d')+'.csv', 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',',
+    csvwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for entry in soup.findAll('tr', {'class': 'field_bg'}):
-	    id = entry.find('a')['href'].split('/')[-1]
-	    email = entry.find('td', {'class':'email'}, 'a').text
-	
-	    names = entry.findAll('td', {'class':'name'})
-	    firstname =  names[0].text
-	    surname =  names[1].text
-	    spamwriter.writerow([id, email, firstname, surname])
+        id = entry.find('a')['href'].split('/')[-1]
+        email = entry.find('td', {'class':'email'}, 'a').text
+        
+        names = entry.findAll('td', {'class':'name'})
+        firstname =  names[0].text
+        surname =  names[1].text
+        csvwriter.writerow([id, email, firstname, surname])
